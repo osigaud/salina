b70839414e6417c431753a12b9479851fbefe980
osigaud <Olivier.Sigaud@upmc.fr>
2022-04-16 13:43:35 +0200

    added load/save to Agent

--- salina/agent.py
+++ salina/agent.py
@@ -148,6 +148,35 @@ class Agent(nn.Module):
         return []
 
 
+    def save_model(self, filename) -> None:
+        """
+        Save a neural network model into a file
+        :param filename: the filename, including the path
+        :return: nothing
+        """
+        torch.save(self, filename)
+
+    def load_model(self, filename):
+        """
+        Load a neural network model from a file
+        :param filename: the filename, including the path
+        :return: the resulting pytorch network
+        """
+        net = torch.load(filename)
+        net.eval()
+        return net
+
+    def update(self, loss) -> None:
+        """
+        Apply a loss to a network using gradient backpropagation
+        :param loss: the applied loss
+        :return: nothing
+        """
+        self.optimizer.zero_grad()
+        loss.sum().backward()
+        self.optimizer.step()
+
+
 class TAgent(Agent):
     """ `TAgent` is used as a convention to represent agents that use a time index in their `__call__` function (not mandatory)
     """
